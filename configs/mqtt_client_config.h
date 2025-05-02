66796cf69c97dfef896d17b7ce1bfef6ba51220b
SabolSocare <130473310+SabolSocare@users.noreply.github.com>
2025-05-02 13:04:15 +0700

    Update change mqtt address in mqtt_client_config.h

--- configs/mqtt_client_config.h
+++ configs/mqtt_client_config.h
@@ -51,7 +51,7 @@
 
 /***************** MQTT CLIENT CONNECTION CONFIGURATION MACROS *****************/
 /* MQTT Broker/Server address and port used for the MQTT connection. */
-#define MQTT_BROKER_ADDRESS             "mqtt.thailand-smartliving.com"
+#define MQTT_BROKER_ADDRESS             "mqtt.kin-yoo-dee.com"
 #define MQTT_PORT                        8883
 
 /* Set this macro to 1 if a secure (TLS) connection to the MQTT Broker is  
@@ -169,85 +169,15 @@
 
 /* Configure the below credentials in case of a secure MQTT connection. */
 /* PEM-encoded client certificate */
-#define CLIENT_CERTIFICATE      \
-"-----BEGIN CERTIFICATE-----\n"\
-"MIIC8TCCAdkCCQCm3BeClQPi4jANBgkqhkiG9w0BAQsFADAlMSMwIQYDVQQDDBoq\n"\
-"LnRoYWlsYW5kLXNtYXJ0bGl2aW5nLmNvbTAeFw0yMzAzMDcxMDU5MTNaFw0yNDAz\n"\
-"MDYxMDU5MTNaMFAxDTALBgNVBAMMBEJpaWwxDDAKBgNVBAoMA0JVVTERMA8GA1UE\n"\
-"CwwIRUFTVFBhcmsxCzAJBgNVBAYTAlRIMREwDwYDVQQHDAhCYW5nc2FlbjCCASIw\n"\
-"DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAN7/vi0GIpnQkVCtACzA+qg1IvUc\n"\
-"8bG+QKEXFie4r/8qAEbSHY8ZQub4UBxDNiFo6oO0GA558L7v4vriZjLxKoBi2UZt\n"\
-"RiTHKUt/V9Cb1gLBPqIDYW/VU6WLFnCxgf2bRvsnzqo2jxhkaLEsBxXT+wKKF0Ol\n"\
-"7BP90kCuJBSaqNxaplUixeYXw+PJEcBuq9qzeaIoqWUIOErikZtX8gA1/OK/yr5S\n"\
-"DmfEN/Fdf0jMf1JtMRLb5NcNP8ibKC0+0jMSNVo/UlW1QdlUCJ2yW9Ckaj+PrSWe\n"\
-"Wyl+Ajr4R2yGDnaLTaRQQGEk7CTYJbHJByhBXFB5LmKIv8p4hMWOWDUQ+csCAwEA\n"\
-"ATANBgkqhkiG9w0BAQsFAAOCAQEAgfMas3BuEa29PUNMSankBoXKjVcRrONmiz0Z\n"\
-"EDYuMJ58lMDSInp+EpJMC95ASVnlxwge7uDwnkRovLVXDqdHq97XPlcrT3SmwEhy\n"\
-"0tQPUdJg02WSr1gKD3JvmTYcEhgVlD29CBl2rotrM4NO5bSIS3mj9jb1kxNCsSvT\n"\
-"UwMEzW700ijrUlvab9UvXgbOjSzpu4tEzv508znDPCdi1TlA17Ul4HqvSSteCCq/\n"\
-"fWQr3PYCLq8hap2uII6ElEaRpxH6y0I0ypHEkZddG+/Z71xkP7vJZ9aC5dwfXSdO\n"\
-"mXEB5Q9TO9NaqZ8zcP3lJ89EDMFiHW1nxJ7QbhJqk1SgE47TZw==\n"\
-"-----END CERTIFICATE-----\n"
 
+//#define CLIENT_CERTIFICATE      \
 
 /* PEM-encoded client private key */
-#define CLIENT_PRIVATE_KEY          \
-"-----BEGIN PRIVATE KEY-----\n"\
-"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDe/74tBiKZ0JFQ\n"\
-"rQAswPqoNSL1HPGxvkChFxYnuK//KgBG0h2PGULm+FAcQzYhaOqDtBgOefC+7+L6\n"\
-"4mYy8SqAYtlGbUYkxylLf1fQm9YCwT6iA2Fv1VOlixZwsYH9m0b7J86qNo8YZGix\n"\
-"LAcV0/sCihdDpewT/dJAriQUmqjcWqZVIsXmF8PjyRHAbqvas3miKKllCDhK4pGb\n"\
-"V/IANfziv8q+Ug5nxDfxXX9IzH9SbTES2+TXDT/ImygtPtIzEjVaP1JVtUHZVAid\n"\
-"slvQpGo/j60lnlspfgI6+Edshg52i02kUEBhJOwk2CWxyQcoQVxQeS5iiL/KeITF\n"\
-"jlg1EPnLAgMBAAECggEANt4b774Ls2bS6lO2sExHIC36zO5zNXWHN1uT0vApNZIj\n"\
-"AUnfhf8ELAo6w8lxvySuVvtv09gC+SfS5EHiPXSDLZWkuiRW1uO98FqDw8skDAd6\n"\
-"HqjQR5DohNnFCrNSNZEIFPHFcrH74VeLPtSDZcaDgytw7+8aOA0Ct3FhZURsedLK\n"\
-"cABnaL+7NLuA+wnEeT2D+dOi7LWnRdCja8VBteKLOZJDnrKUEfpMk/BJ9qGiRu1/\n"\
-"b7z+G/XypaxeQcvqu3yQnLaWcn6wzpnM6YolER5k/0CGTlV414zJ7J/ECvX+LNGv\n"\
-"s+KWF/DDSwHjYVVKu6cx8DerOdOCGVf1bZWxJS0qcQKBgQDpS0KuP+WPhhEqpdea\n"\
-"LX28y1JGQ986PKgfGf4KB7TYnYQJEc8hkG7PmYr6+soiwE13Fm38RfgHSNeEdH52\n"\
-"kuWtFM5ZaPvcytnmQHAmLGCLisUFPrZVD8MJOpWdhAAQHVdGSohYe5KCdNc87FW3\n"\
-"HP9NfwgxYv9gwY5Uk8PnDQXrAwKBgQD0s/kXK6Wz9DIGd1duFyqSy1FRnONkdsny\n"\
-"SNJtyXnOJDgygUcr9IYPAVUI4t5jW1br5XdDB1ujCGiPaeiYWEhosIWDaNMPt/WT\n"\
-"30AKcGAINiwoPtzcOCeSmE62gJU3aOABOrkP6WZ69tbZAMCcmUkdATPXxu1X6Kt3\n"\
-"R0jyLrjXmQKBgHnV1iMON6L2zyxIpApey5W3UWZkt8XAO8TSXDt8vtOJn5Tk+hl7\n"\
-"RoOgmlZsdvgjGdGhoxrmp5uWTQddl1XxbYUwontLOo2aWqXG7UriTiFR/Ya5s5tE\n"\
-"ugSt1ckHUMkZqaSJ9B7RmjgLMQUeH72bpTEhsHOqUdiD/QLqkJqv0HFlAoGBANdH\n"\
-"0rgAzVpBVxV7Pu768JiOyS8M4L0NCOPgD0lJPKEQGmTMeYoBKUPgPyh/N3mf7RvC\n"\
-"GCdfPM504lBxs5D78lAwJHYU7cLha+s5OMU2UjZHOpf5niMcQCxnVLCY9S33VUOf\n"\
-"nxqRk3hQ23m0gMAE117uJOcL+tkhOGDK1TFDy5jZAoGADcgwU0HgGSzpNPLy0CEf\n"\
-"iPEZGRGdMXM1woJk7FdKg1MdzzkJCYJqjpGugyDD6FmPs6FydeORyfYoOzcyOGl7\n"\
-"SXn/7kgRmNoRJXbB7qufanOy21GXXhxQmZZpxmaaZ/ZO00dS61yI7pAvCk1UR0u9\n"\
-"+qYB96Rqsz2niPeokgkMAzQ=\n"\
-"-----END PRIVATE KEY-----\n"
+//#define CLIENT_PRIVATE_KEY          \
 
 /* PEM-encoded Root CA certificate */
-#define ROOT_CA_CERTIFICATE     \
-"-----BEGIN CERTIFICATE-----\n"\
-"MIIEMjCCAxqgAwIBAgIBATANBgkqhkiG9w0BAQUFADB7MQswCQYDVQQGEwJHQjEb\n"\
-"MBkGA1UECAwSR3JlYXRlciBNYW5jaGVzdGVyMRAwDgYDVQQHDAdTYWxmb3JkMRow\n"\
-"GAYDVQQKDBFDb21vZG8gQ0EgTGltaXRlZDEhMB8GA1UEAwwYQUFBIENlcnRpZmlj\n"\
-"YXRlIFNlcnZpY2VzMB4XDTA0MDEwMTAwMDAwMFoXDTI4MTIzMTIzNTk1OVowezEL\n"\
-"MAkGA1UEBhMCR0IxGzAZBgNVBAgMEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4GA1UE\n"\
-"BwwHU2FsZm9yZDEaMBgGA1UECgwRQ29tb2RvIENBIExpbWl0ZWQxITAfBgNVBAMM\n"\
-"GEFBQSBDZXJ0aWZpY2F0ZSBTZXJ2aWNlczCCASIwDQYJKoZIhvcNAQEBBQADggEP\n"\
-"ADCCAQoCggEBAL5AnfRu4ep2hxxNRUSOvkbIgwadwSr+GB+O5AL686tdUIoWMQua\n"\
-"BtDFcCLNSS1UY8y2bmhGC1Pqy0wkwLxyTurxFa70VJoSCsN6sjNg4tqJVfMiWPPe\n"\
-"3M/vg4aijJRPn2jymJBGhCfHdr/jzDUsi14HZGWCwEiwqJH5YZ92IFCokcdmtet4\n"\
-"YgNW8IoaE+oxox6gmf049vYnMlhvB/VruPsUK6+3qszWY19zjNoFmag4qMsXeDZR\n"\
-"rOme9Hg6jc8P2ULimAyrL58OAd7vn5lJ8S3frHRNG5i1R8XlKdH5kBjHYpy+g8cm\n"\
-"ez6KJcfA3Z3mNWgQIJ2P2N7Sw4ScDV7oL8kCAwEAAaOBwDCBvTAdBgNVHQ4EFgQU\n"\
-"oBEKIz6W8Qfs4q8p74Klf9AwpLQwDgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQF\n"\
-"MAMBAf8wewYDVR0fBHQwcjA4oDagNIYyaHR0cDovL2NybC5jb21vZG9jYS5jb20v\n"\
-"QUFBQ2VydGlmaWNhdGVTZXJ2aWNlcy5jcmwwNqA0oDKGMGh0dHA6Ly9jcmwuY29t\n"\
-"b2RvLm5ldC9BQUFDZXJ0aWZpY2F0ZVNlcnZpY2VzLmNybDANBgkqhkiG9w0BAQUF\n"\
-"AAOCAQEACFb8AvCb6P+k+tZ7xkSAzk/ExfYAWMymtrwUSWgEdujm7l3sAg9g1o1Q\n"\
-"GE8mTgHj5rCl7r+8dFRBv/38ErjHT1r0iWAFf2C3BUrz9vHCv8S5dIa2LX1rzNLz\n"\
-"Rt0vxuBqw8M0Ayx9lt1awg6nCpnBBYurDC/zXDrPbDdVCYfeU0BsWO/8tqtlbgT2\n"\
-"G9w84FoVxp7Z8VlIMCFlA2zs6SFz7JsDoeA3raAVGI/6ugLOpyypEBMs1OUIJqsi\n"\
-"l2D4kF501KKaU73yqWjgom7C12yxow+ev+to51byrvLjKzg6CYG1a4XXvi3tPxq3\n"\
-"smPi9WIsgtRqAEFQ8TmDn5XpNpaYbg==\n"\
-"-----END CERTIFICATE-----\n"
+//#define ROOT_CA_CERTIFICATE     \
+
 
 
 /******************************************************************************
